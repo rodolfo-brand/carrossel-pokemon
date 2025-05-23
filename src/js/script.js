92e7f0dd570f27c1c74d6e3922c294947c2d8b6b
rodolfo-brand <rnbrandalise@gmail.com>
2025-05-23 17:02:30 -0300

    started creating come classes for the future

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+/*
+passo a passo do qyue planejo fazer com o script:
+1. Selecionar todos os elementos com a classe "cartao". 2. Selecionar os botões "Avançar" e "Voltar" pelos seus IDs.
+3. Criar uma variável para controlar o cartão atual.
+4. Criar funções para esconder e mostrar os cartões.
+5. Adicionar os event listeners aos botões para executar as funções.
+*/
